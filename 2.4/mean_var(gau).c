ecfb83d77654102d5277265081c37f05ee0c1161
Pranavb060504 <102507651+Pranavb060504@users.noreply.github.com>
2022-06-25 13:49:37 +0530

    Create mean_var(gau).c

---
+++ 2.4/mean_var(gau).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int count=0;
+    double sum=0;
+    double squares=0;
+    double t;
+    double a,b;
+    FILE*fp=fopen("gau.dat","r");
+    while(fscanf(fp, "%lf", &t)!=-1){
+        count+=1;
+        sum=sum+t;
+        squares=squares+(t*t);
+    }
+    a=sum/count;
+    b=(squares/count)-(a*a);
+
+    printf("Mean:%lf,Variance:%lf",a,b);
+    fclose(fp);
+}
