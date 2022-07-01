0b498f70c9314806566e9c9cdf1e8a8d94e9d346
Pranavb060504 <102507651+Pranavb060504@users.noreply.github.com>
2022-07-01 13:16:22 -0400

    Add files via upload

---
+++ 4.1/T_gen_dat.py
@@ -0,0 +1,23 @@
+with open("uni1.dat","r") as f1:
+    with open ("uni2.dat","r")as f2:
+        with open ("T.dat","w") as f3:
+            a=[]
+            for line in f1:
+                a.append(line)
+            b=[]
+            for line in f2:
+                b.append(line)
+            c=[]
+            for i in range(0,len(b)):
+                c.append(float(a[i])+float(b[i]))    
+            for i in range(0,len(c)):
+                f3.write(f"{c[i]}\n")
+            print(len(a),len(b),len(c))
+
+                
+
+            
+                     
+
+
+
