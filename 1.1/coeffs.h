67ecc9e7f913d96363eb9c1106a3a59ba72d088f
Pranavb060504 <102507651+Pranavb060504@users.noreply.github.com>
2022-07-02 00:53:55 -0400

    Add files via upload

--- 1.1/coeffs.h
+++ 1.1/coeffs.h
@@ -1,4 +1,6 @@
 //Function declaration
+#include<stdlib.h>
+#include<math.h>
 double **createMat(int m,int n);
 void readMat(double **p, int m,int n);
 void print(double **p,int m,int n);
@@ -11,6 +13,7 @@ double **transpose(double **a,  int m, int n);
 void uniform(char *str, int len);
 void gaussian(char *str, int len);
 double mean(char *str);
+double var(char*str);
 //End function declaration
 
 
@@ -251,3 +254,17 @@ fclose(fp);
 
 }
 //End function for generating Gaussian random numbers
+double var(char*str){
+FILE* fp=fopen(str,"r");
+double b;
+int count=0;
+double t;
+double squares=0;
+while(fscanf(fp, "%lf", &t)!=-1){
+        count+=1;
+        squares=squares+(t*t);
+    }
+    b=(squares/count)-(mean(str)*mean(str));
+    fclose(fp);
+    return b;
+}
