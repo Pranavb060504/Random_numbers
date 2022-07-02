af02be5e4ed55d80c29071a0605592392916333e
Pranavb060504 <102507651+Pranavb060504@users.noreply.github.com>
2022-07-02 00:54:26 -0400

    Add files via upload

--- 1.4/mean_var.c
+++ 1.4/mean_var.c
@@ -1,20 +1,9 @@
 #include<stdio.h>
+#include"coeffs.h"
 int main(){
-    int count=0;
-    double sum=0;
-    double squares=0;
-    double t;
-    double a,b;
-    FILE*fp=fopen("uni.dat","r");
-    while(fscanf(fp, "%lf", &t)!=-1){
-        count+=1;
-        sum=sum+t;
-        squares=squares+(t*t);
-    }
-    a=sum/count;
-    b=(squares/count)-(a*a);
-
-    printf("Mean:%lf,Variance:%lf",a,b);
-    fclose(fp);
+     char* str="uni.dat";
+    double m=mean(str);
+   double v=var(str);
+    printf("Mean:%lf,Variance:%lf",m,v);
     //Mean=0.500137, Variance=0.083251
 }
