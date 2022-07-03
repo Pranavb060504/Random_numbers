c8495b6efa3294f4914b697ef9531eb4c04dad4c
Pranavb060504 <102507651+Pranavb060504@users.noreply.github.com>
2022-07-03 09:57:33 +0530

    Update T_pdf.py

--- 4.5/T_pdf.py
+++ 4.5/T_pdf.py
@@ -15,7 +15,7 @@ maxlim=5.0
 x = np.linspace(-maxlim,maxlim,maxrange)#points on the x axis
 simlen = int(1e6) #number of samples
 err = [] #declaring probability list
-pdf = [0] #declaring pdf list
+pdf = [] #declaring pdf list
 h = 2*maxlim/(maxrange-1)
 #randvar = np.random.normal(0,1,simlen)
 #randvar = np.loadtxt('uni.dat',dtype='double')
@@ -33,43 +33,19 @@ for i in range(0,maxrange-1):
 
 def T_pdf(y):
     if(y<0):
-        return 0
+        return 0.00
     if(0<=y<=1):
         return y
     if(1<y<=2):
         return 2-y
     if(y>2):
         return 0
-
-pdf_formula = []
-for i in range(len(x)):
-    pdf_formula.append(T_pdf(x[i]))
       
-plt.plot(x, pdf, 'o')
-plt.plot(x, pdf_formula)
 
+vec_T_pdf = scipy.vectorize(T_pdf)
 
-#vec_T_pdf = scipy.vectorize(T_pdf)
-
-#plt.plot(x[0:(maxrange-1)].T,pdf,'o')
-# a=[]
-# b=[]
-# c=[]
-# d=[]
-# for i in range(0,maxrange):
-#     if x[i]<0:
-#         a.append(x[i])
-#     if 0<=x[i]<=1:
-#         b.append(x[i])
-#     if 1<x[i]<=2:
-#         c.append(x[i])
-#     if x[i]>2:
-#         d.append(x[i])
-# plt.plot(a,vec_T_pdf(a))#plotting the PDF
-# plt.plot(b,vec_T_pdf(b))
-# plt.plot(c,vec_T_pdf(c))
-# plt.plot(d,vec_T_pdf(d))
-#plt.plot(x,vec_T_pdf(x))
+plt.plot(x[0:(maxrange-1)].T,pdf,'o')
+plt.plot(x,vec_T_pdf(x))
 plt.grid() #creating the grid
 plt.xlabel('$x_i$')
 plt.ylabel('$p_X(x_i)$')
