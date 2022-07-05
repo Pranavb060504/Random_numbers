804a909ab0f4f26216f2b9eec09b89fc768cf01a
Pranavb060504 <102507651+Pranavb060504@users.noreply.github.com>
2022-07-05 02:15:53 -0400

    Add files via upload

--- 5.7/Pgen.c
+++ 5.7/Pgen.c
@@ -2,13 +2,16 @@
 int main()
 {
     FILE*fp=fopen("aG.dat","r");
-    FILE*f1=fopen("bernoulli.dat","r");
-    FILE*f2=fopen("gau3.dat","r");
     FILE*f3=fopen("Pgen.dat","w");
-    double p1=0,p2=0,y;
+    double p1,p2,y;
     double t,r,p,x;
-    int count1=0,count2=0;
     while(fscanf(fp,"%lf",&t)!=EOF){
+        p1=0;
+        p2=0;
+        int count1=0;
+        int count2=0;
+        FILE*f1=fopen("bernoulli.dat","r");
+        FILE*f2=fopen("gau3.dat","r");
         while(fscanf(f1,"%lf",&r)!=EOF){
             fscanf(f2,"%lf",&p);
             y=t*r+p;
@@ -25,15 +28,11 @@ int main()
                 p2=p2+1;
             }  
         }
-        x=(double)(0.5*p1)/count2+(double)(0.5*p2)/count1;
-        p1=0;
-        p2=0;
-        f2 = fopen("gau3.dat","r");
-        f1=fopen("bernoulli.dat","r");
+        x=(0.5*p1)/count2+(0.5*p2)/count1;
         fprintf(f3,"%lf\n",x);
+         fclose(f1);
+         fclose(f2);
     }
     fclose(fp);
-    fclose(f1);
-    fclose(f2);
     fclose(f3);
 }
