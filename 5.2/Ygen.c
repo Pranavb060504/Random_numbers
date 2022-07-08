99914f1aa7104496e1d9de7ddbb328472624d65d
Pranavb060504 <102507651+Pranavb060504@users.noreply.github.com>
2022-07-08 01:36:51 -0400

    Update Ygen.c

--- 5.2/Ygen.c
+++ 5.2/Ygen.c
@@ -5,7 +5,7 @@ int main()
     FILE*fp=fopen("gau.dat","r");
     FILE*f1=fopen("bernoulli.dat","r");
     FILE*f2=fopen("Y.dat","w");
-    for(int i=0;i<10000;i++){
+    for(int i=0;i<1000000;i++){
         fscanf(fp,"%f",&a);
         fscanf(f1,"%d",&t);
         fprintf(f2,"%f\n",a+5*t);
