e35177a3fd43f7d8665732b16ce27b074eb81657
Pranavb060504 <102507651+Pranavb060504@users.noreply.github.com>
2022-07-11 11:34:11 +0530

    Delete A_gen.py

--- 6.3/A_gen.py
+++
@@ -1,8 +0,0 @@
-import math
-with open("G.dat","r") as f1:
-    a=[]
-    for line in f1:
-        a.append(math.sqrt(float(line)))
-with open("A.dat","w") as f2:
-    for i in range(0,len(a)):
-        f2.write(f"{a[i]}\n")
