8f7f9468c7afcfe414823e46002de8a4503c3b94
Pranavb060504 <102507651+Pranavb060504@users.noreply.github.com>
2022-07-11 15:34:00 -0400

    Add files via upload

---
+++ 8.1/2d.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+ys0i=np.loadtxt("ys0i.dat",dtype="double")
+ys0j=np.loadtxt("ys0j.dat",dtype="double")
+ys1i=np.loadtxt("ys1i.dat",dtype="double")
+ys1j=np.loadtxt("ys1j.dat",dtype="double")
+plt.plot(ys0i,ys0j,'.',color='r',label='y|s0')
+plt.plot(ys1i,ys1j,'.',color='b',label='y|s1')
+plt.legend()
+plt.show()
