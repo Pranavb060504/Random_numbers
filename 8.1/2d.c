8d50d8d8dfab2ebf55ac970ac6c87dfea5aa45b4
Pranavb060504 <102507651+Pranavb060504@users.noreply.github.com>
2022-07-11 23:48:45 -0400

    Add files via upload

--- 8.1/2d.c
+++ 8.1/2d.c
@@ -11,40 +11,37 @@ vec sum(vec v1,  vec v2){
   v3.i=v1.i+v2.i;
   v3.j=v1.j+v2.j;
   return v3;
-  }
+}
 vec sp(vec v1,double k){
     vec v3;
     v3.i=v1.i*k;
     v3.j=v1.j*k;
+    return v3;
 }
 int main(){
     vec s0,s1;
     s0.i=1;
-    s0.j=0;
-    s1.i=0;
+    s0.j=-1;
+    s1.i=-1;
     s1.j=1;
     int a=10;
-    FILE*fp=fopen("ys0i.dat","w");
-    FILE*f1=fopen("ys0j.dat","w");
-    FILE*f2=fopen("ys1i.dat","w");
-    FILE*f3=fopen("ys1j.dat","w");
-    for(int i=0;i<N;i++){
+    FILE*fp=fopen("./ys0i.dat","w");
+    FILE*f1=fopen("./ys0j.dat","w");
+    FILE*f2=fopen("./ys1i.dat","w");
+    FILE*f3=fopen("./ys1j.dat","w");
+    for(int p=0; p<N; p++){
         vec y;
         vec n;
         n.i=gau();
         n.j=gau();
         int x=bl();
-        if(x==1){
+        if(x == 1){
             y=sum(sp(s1,a),n);
-        }
-        if(x==-1){
-            y=sum(sp(s0,a),n);
-        }
-        if(y.i>y.j && x==1){
             fprintf(fp,"%lf\n",y.i);
             fprintf(f1,"%lf\n",y.j);
         }
-        if(y.i<y.j && x==-1){
+        if(x==-1){
+            y=sum(sp(s0,a),n);
             fprintf(f2,"%lf\n",y.i);
             fprintf(f3,"%lf\n",y.j);
         }
